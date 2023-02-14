6aae2779d900300980a7ae0b60e0cd3e8d73542a
roshanpatil110598 <patilroshan503@gmail.com>
2023-02-14 15:14:16 +0530

    FFS initial

---
+++ FFS_Project/src/test/java/com/ffs/main/FfsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffs.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
